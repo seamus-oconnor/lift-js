5f259dea480e78ca94eafc671afa6e1c3cc80657
Séamus O'Connor <seamus.oconnor@pneumaticweb.com>
2013-12-10 09:45:48 -0800

    Changed transitionEnd test to check for browser support of CSS transitions in general before testing. Now IE 9 passes the mocha tests by reporting that it doesn't support CSS transitions (before it was hanging and failing).

--- test/spec/dom/node/transitionend.js
+++ test/spec/dom/node/transitionend.js
@@ -1,17 +1,30 @@
 describe("DOM Element #transitionEnd", function() {
+  var support = false;
   var prefixes = ['msT', 'WebkitT', 'OT', 'MozT', 't'];
-  it("should fire transitionEnd", function(done) {
-    var a = document.createElement('a');
-    a.style.color = 'black';
-    for (var i = prefixes.length - 1; i >= 0; i--) {
-      a.style[prefixes[i] + 'ransition'] = '10ms color';
+  var div = document.createElement('a');
+  for(var i = prefixes.length - 1; i >= 0; i--) {
+    if((prefixes[i] + 'ransition') in div.style) {
+      support = true;
     }
-    a.addEventListener('transitionend', function() {
-      done();
-    }, false);
-    document.body.appendChild(a);
-    document.body.offsetTop; // flush layout
-    a.style.color = 'white';
-    setTimeout(function() { document.body.removeChild(a); }, 100);
-  });
+  }
+
+  if(support) {
+    it("should fire transitionEnd", function(done) {
+      var a = document.createElement('a');
+      a.style.color = 'black';
+      for(var i = prefixes.length - 1; i >= 0; i--) {
+        a.style[prefixes[i] + 'ransition'] = '10ms color';
+      }
+      a.addEventListener('transitionend', function() {
+        done();
+      }, false);
+      wasteland.appendChild(a);
+      document.body.offsetTop; // flush layout
+      a.style.color = 'white';
+    });
+  } else {
+    it("doesn't support CSS transitions", function() {
+    });
+  }
+
 });
